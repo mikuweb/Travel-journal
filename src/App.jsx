55e4736cea63a73b92d02330a6cbe3f1d55faf28
mikuweb <mxxx.kwtmg@gmail.com>
2022-12-15 09:42:56 -0800

    Add props

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { Card } from './components/Card'
-import { Header } from './components/Header'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { Card } from "./components/Card";
+import { Header } from "./components/Header";
+import data from "./data";
+
+const cardDisplay = data.map((item) => {
+  return <Card key={item.id} item={item} />;
+});
 
 function App() {
-  return (<div className='container'>
-    <Header />
-    <Card />
-    <Card />
-    <Card />
-  </div>)
+  console.log(data)
+  return (
+    <div className="container">
+      <Header />
+      {cardDisplay}
+    </div>
+  );
 }
 
-export default App
-
-
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,22 @@
-export const Card = () => {
+
+export const Card = (props) => {
+  
   return (
     <div className="card-container">
       <div className="card-left">
-        <img src="./images/mountfuji.png" />
+        <img src={props.item.imageUrl} />
       </div>
       <div className="card-right">
-      <div className="card-flex">
-        <div className="card-country">JAPAN</div>
-        <div className="card-googlelink">View on Google Maps</div>
+        <div className="card-flex">
+          <div className="card-country">{props.item.location}</div>
+          <div className="card-googlelink">
+            <a href={props.item.googleMapsUrl}>View on Google Maps</a>
+          </div>
         </div>
-        <div className="card-title">Mount Fuji</div>
-        <div className="card-date">12 Jan, 2021 - 24 Jan, 2021</div>
+        <div className="card-title">{props.item.title}</div>
+        <div className="card-date">{`${props.item.startDate} - ${props.item.endDate}`}</div>
         <div className="card-comment">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
+          {`${props.item.description}`}
         </div>
       </div>
     </div>
